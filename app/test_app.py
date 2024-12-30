c25b6f60693dd8a6c86faf273389f36da7d0b39a
VictorSordi <victorsordi200301@outlook.com>
2024-12-30 19:05:41 -0300

    Alterado test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -1,25 +1,20 @@
-# test_app.py
 import platform
 from io import StringIO
 import sys
 import pytest
 
-# Importando a função do app.py para testar
 from app import os_name, architecture
 
 
 def test_os_name():
-    # Verificar se o nome do sistema operacional está correto
     assert os_name == platform.system()
 
 
 def test_architecture():
-    # Verificar se a arquitetura está correta
     assert architecture == platform.architecture()
 
 
 def test_output():
-    # Teste se a saída está correta
     captured_output = StringIO()
     sys.stdout = captured_output
     print(f"Hello World! from {os_name} operating system on {
