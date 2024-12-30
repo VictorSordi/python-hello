9cf85efb14056d564597c4cd44e74a681e9a6868
VictorSordi <victorsordi200301@outlook.com>
2024-12-30 19:20:15 -0300

    Alterado test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -5,22 +5,17 @@ import pytest
 
 from app import os_name, architecture
 
-
 def test_os_name():
     assert os_name == platform.system()
-
 
 def test_architecture():
     assert architecture == platform.architecture()
 
-
 def test_output():
     captured_output = StringIO()
     sys.stdout = captured_output
-    print(f"Hello World! from {os_name} operating system on {
-          architecture[0]} {architecture[1]} architecture")
+    print(f"Hello World! from {os_name} operating system on {architecture[0]} {architecture[1]} architecture")
     sys.stdout = sys.__stdout__
 
-    expected_output = f"Hello World! from {os_name} operating system on {
-        architecture[0]} {architecture[1]} architecture\n"
+    expected_output = f"Hello World! from {os_name} operating system on {architecture[0]} {architecture[1]} architecture\n"
     assert captured_output.getvalue() == expected_output
